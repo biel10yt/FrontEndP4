2815b636bffee0e26edaf1eea192e0f5c6f3812c
Gabriel Soares <gabrielsoares25022005@gmail.com>
2024-08-21 21:29:15 -0300

    updated

--- aula01/src/componets/Dividir.jsx
+++ aula01/src/componets/Dividir.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dividir = (props) => {
   return (
     <div>
-        <h1>O resultado de num1 / num2 é:{props.num1 / props.num2}</h1>
+        <h1>O resultado de {props.num1} / {props.num2} é:{props.num1 / props.num2}</h1>
     </div>
   )
 }
