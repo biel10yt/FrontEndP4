ea7414d7328afca7e8a860ad0672ee55816b7be0
Gabriel Soares <gabrielsoares25022005@gmail.com>
2024-11-24 13:54:46 -0300

    aula05

---
+++ aula05/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GerenciamentoAlunos from './assets/components/GerenciamentoAlunos'
+
+const App = () => {
+  return (
+    <div>
+        <GerenciamentoAlunos/>
+    </div>
+  )
+}
+
+export default App
